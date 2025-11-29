70b192eb5d4cf3d218cf8b9b9b304107bdc5971e
Alessandro Cauã Macedo Marciel <70543246+alessandro-caua@users.noreply.github.com>
2025-11-29 15:09:24 -0300

    Add files via upload

---
+++ n2do2Alessandro/app/src/main/java/com/outracoisa/n2do2alessandro/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.outracoisa.n2do2alessandro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.outracoisa.n2do2alessandro.ui.theme.N2do2AlessandroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            N2do2AlessandroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    N2do2AlessandroTheme {
+        Greeting("Android")
+    }
+}
